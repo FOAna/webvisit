8fb9bd681b8a7b19142760a919286a95a6fc0487
FOAna <kristendamon@yandex.ru>
2021-06-09 23:52:49 +0500

    made header great again

--- webvisit/src/App.js
+++ webvisit/src/App.js
@@ -4,7 +4,31 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App__header"></header>
+      <header className="App__header">
+        <div className="App__headBackground"></div>
+        <div className="App__headIcons">
+          <img
+            className="App__icon"
+            src="https://img.icons8.com/clouds/100/000000/up.png"
+          />
+          <img
+            className="App__icon"
+            src="https://img.icons8.com/clouds/100/000000/cat.png"
+          />
+          <img
+            className="App__icon"
+            src="https://img.icons8.com/clouds/100/000000/business.png"
+          />
+          <img
+            className="App__icon"
+            src="https://img.icons8.com/clouds/100/000000/programming-flag.png"
+          />
+          <img
+            className="App__icon"
+            src="https://img.icons8.com/clouds/100/000000/chat.png"
+          />
+        </div>
+      </header>
       <main className="App__main">
         <img className="App__background" src={night} />
       </main>
